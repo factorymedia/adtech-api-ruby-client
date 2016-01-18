25aafa391ae6fa3cc50706cc5e5c11b74c2d7d0b
Min Kim <minsikzzang@gmail.com>
2016-01-18 15:33:55 +0000

    Add to support advertiser / customer category report

--- lib/adtech/api/report.rb
+++ lib/adtech/api/report.rb
@@ -7,25 +7,22 @@ module ADTech
       def get_report_url(report_type_id, start_date, end_date, entities)
         report = client.reportService.getReportById(report_type_id)
 
-        ADTech.logger.info "Your report (#{report_type_id}) is of entity type: #{report.getEntityType} and report category: #{report.getReportCategory()}"
+        ADTech.logger.info "Your report (#{report_type_id}) is of entity type: \
+#{report.getEntityType} and report category: #{report.getReportCategory}"
 
         start_cal = gregoian_calendar(start_date.year,
                                       start_date.month - 1,
                                       start_date.day,
-                                      0,
-                                      0,
-                                      0)
+                                      0, 0, 0)
         ADTech.logger.info "Report start date set to: #{start_cal.getTime}";
 
         end_cal = gregoian_calendar(end_date.year,
                                     end_date.month - 1,
                                     end_date.day,
-                                    23,
-                                    59,
-                                    59)
+                                    23, 59, 59)
         ADTech.logger.info "Report end date set to: #{end_cal.getTime}";
 
-        entities = Admin.new(client).get_website_ids if entities.empty? || !entities
+        entities = default_entities(report.getReportCategory) if entities.empty? || !entities
 
         report_queue_entry = client.reportService.requestReportByEntities(
           report_type_id,
@@ -85,6 +82,17 @@ module ADTech
 
       private
 
+      def default_entities(category)
+        entities = case category
+        when 'website'
+          Admin.new(client).website_ids
+        when 'customer'
+          Admin.new(client).customer_ids
+        when 'advertiser'
+          Admin.new(client).advertiser_ids
+        end
+      end
+
       def gregoian_calendar(year, month, day, hour, minute, second)
         cal = GregorianCalendar.getInstance()
         cal.set(Calendar::DAY_OF_MONTH, day)
@@ -124,6 +132,8 @@ module ADTech
           IReport::REPORT_CATEGORY_PAGE
         when 'placement'
           IReport::REPORT_CATEGORY_PLACEMENT
+        when 'customer'
+          IReport::REPORT_CATEGORY_CUSTOMER
         when 'advertiser'
           IReport::REPORT_CATEGORY_ADVERTISER
         when 'bannersize'
