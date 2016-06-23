c26770927160eec6aea69b36448abaeafae0ca84
Min Kim <minsikzzang@gmail.com>
2016-06-23 12:51:33 +0100

    Fix issue for Gregorian Calendar / add specs

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -145,4 +145,20 @@ describe ADTech::API::Report do
       end
     end
   end
+
+  describe '.gregorian_calendar' do
+    context 'when given time is end of day' do
+      it 'should return relevant gregorian_calendar object' do
+        cal = @report.send(:gregorian_calendar, 2016, 5, 12, 23, 59, 59)
+        expect(cal.getTime.to_s). to eq('Sun Jun 12 23:59:59 BST 2016')
+      end
+    end
+
+    context 'when given time is beginnig of day' do
+      it 'should return relevant gregorian_calendar object' do
+        cal = @report.send(:gregorian_calendar, 2016, 5, 12, 0, 0, 0)
+        expect(cal.getTime.to_s). to eq('Sun Jun 12 00:00:00 BST 2016')
+      end
+    end
+  end
 end
